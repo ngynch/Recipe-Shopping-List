c723c240e648d9819341525852d169c3a9742228
Cong Huan Nguyen <conghuannguyen1996@gmail.com>
2023-03-19 16:40:23 +0100

    Add create recipe button Add syntax validation of ingredients

--- Frontend/src/Modal/RecipeModal/CreateRecipeModal.jsx
+++ Frontend/src/Modal/RecipeModal/CreateRecipeModal.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { Button, darkScrollbar } from "@mui/material";
-
 import Ingredient from "./Ingredient";
+import { isValid } from "../../Utils/IngredientUtils";
+import axios from "axios";
 
 function CreateRecipeModal(props) {
-  const [count, setCount] = useState(1);
+  const [recipeName, setRecipeName] = useState("");
+  const [ingredientList, setIngredientList] = useState([
+    { name: "", amount: "", unit: "pcs" },
+  ]);
 
   const handleClose = () => {
     props.setShowModal(false);
@@ -24,42 +28,70 @@ function CreateRecipeModal(props) {
     p: 3,
   };
 
-  const handleChange = () => {
-    debugger;
-    document.getElementById();
+  const verifySyntax = () => {
+    console.log(ingredientList);
+    return ingredientList.every(
+      (ingredient) => isValid(ingredient.amount) && ingredient.name.length > 0
+    );
   };
 
   const createRecipe = () => {
-    alert("hi");
+    if (!verifySyntax()) {
+      alert("check your ingredients");
+    } else {
+      axios.post(process.env.REACT_APP_BACKEND + "/recipe/add", {
+        name: recipeName,
+        ingredients: ingredientList,
+      });
+      handleClose();
+    }
   };
 
-  const ingredients = [];
-  for (let i = 1; i <= count; i++) {
-    ingredients.push(<br />);
-    ingredients.push(<Ingredient />);
-  }
+  const addIngredient = () => {
+    setIngredientList([
+      ...ingredientList,
+      { name: "", amount: "1", unit: "pcs" },
+    ]);
+  };
+
+  const removeIngredient = () => {
+    setIngredientList(ingredientList.slice(0, -1));
+  };
+
+  const testButton = () => {
+    console.log(ingredientList);
+  };
+
+  const handleIngredientUpdate = (key, type, newValue) => {
+    let newIngredientList = ingredientList;
+    newIngredientList[key][type] = newValue;
+    setIngredientList(newIngredientList);
+    console.log(ingredientList);
+  };
+
   return (
     <Modal open={props.showModal} onClose={handleClose}>
       <Box sx={style}>
-        <TextField label="Recipe Name" />
+        <TextField
+          label="Recipe Name"
+          onChange={(event) => setRecipeName(event.target.value)}
+        />
         <br />
-        {ingredients}
         <br />
-        <Button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </Button>
-        {count > 1 && (
-          <Button
-            onClick={() => {
-              setCount(count - 1);
-            }}
-          >
-            -
-          </Button>
+        {ingredientList.map((ingredient, index) => (
+          <Ingredient
+            key={index}
+            keyProp={index}
+            handleIngredientUpdate={handleIngredientUpdate}
+            name={ingredient.name}
+            amount={ingredient.amount}
+            unit={ingredient.unit}
+          ></Ingredient>
+        ))}
+        <br />
+        <Button onClick={addIngredient}>+</Button>
+        {ingredientList.length > 1 && (
+          <Button onClick={removeIngredient}>-</Button>
         )}
         <br />
         <br />
